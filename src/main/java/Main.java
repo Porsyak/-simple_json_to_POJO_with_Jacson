08f61195db7e877e293b08ca29c77167f65f4b06
Ivan Porsyak <2663634@gmail.com>
2022-08-31 13:04:55 +0300

    Add method calcNumberOfEmployees2 with StreamAPI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,6 +7,8 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,12 +19,13 @@ public class Main {
             List<Root> rootList = List.of(om.readValue(reader, Root[].class));
             //System.out.println(rootList);
             calcNumberOfEmployees(rootList, 20_000);
+            System.out.println(calcNumberOfEmployees2(rootList, 20_000));
         } catch (IOException e) {
             e.getStackTrace();
         }
     }
 
-    public static  void calcNumberOfEmployees(List<Root> departments, long threshold) {
+    public static void calcNumberOfEmployees(List<Root> departments, long threshold) {
 
 
         departments.forEach(new Consumer<Root>() {
@@ -30,17 +34,26 @@ public class Main {
                 root.employees().forEach(new Consumer<Employee>() {
                     @Override
                     public void accept(Employee employee) {
-                        if(employee.salary() == threshold){
+                        if (employee.salary() == threshold) {
                             System.out.println(employee.name());
                         }
                     }
                 });
             }
         });
+    }
 
-
+    public static long calcNumberOfEmployees2(List<Root> departments, long threshold) {
+        return departments.stream()
+                .flatMap((Function<Root, Stream<Employee>>) root -> root.employees().stream())
+                .filter(employee -> employee.salary() == threshold)
+                .count();
     }
 }
 
-record Employee(String name, int salary){}
-record Root(String name, String code, List<Employee> employees){}
+
+record Employee(String name, int salary) {
+}
+
+record Root(String name, String code, List<Employee> employees) {
+}
